7ddc62d5e18bc53e8703895a823027cee41e61f0
kinglouitch <tho_schaer@hotmail.com>
2015-03-31 22:11:15 +0200

    adding gender to person

---
+++ stammbaum-model/src/main/java/com/schaer/stammbaum/model/Gender.java
@@ -0,0 +1,6 @@
+package com.schaer.stammbaum.model;
+
+public enum Gender {
+	MALE,
+	FEMALE;
+}
--- stammbaum-model/src/main/java/com/schaer/stammbaum/model/Person.java
+++ stammbaum-model/src/main/java/com/schaer/stammbaum/model/Person.java
@@ -6,18 +6,52 @@ import org.joda.time.LocalDate;
 
 public class Person {
 
+	private Gender gender;
 	private String firstName;
 	private String lastName;
+	private String unmarriedLastName;
 	private LocalDate birthDate;
 	private LocalDate deathDate;
 	private List<Person> biologicalChildren;
 	private List<Person> adoptedChildren;
 	
-	public Person(String vorname) {
-		this.firstName = vorname;
+	public Person(Gender gender) {
+		this.gender = gender;
 	}
 
 	public String getVorname() {
 		return firstName;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getUnmarriedLastName() {
+		return unmarriedLastName;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public LocalDate getDeathDate() {
+		return deathDate;
+	}
+
+	public List<Person> getBiologicalChildren() {
+		return biologicalChildren;
+	}
+
+	public List<Person> getAdoptedChildren() {
+		return adoptedChildren;
 	}
 }
